6dabf7e998e55f073cb3c33d5233b93849d97f88
buckstarz <kevinchenier1995@gmail.com>
2021-03-23 22:05:50 -0400

    Added speaker

--- main.py
+++ main.py
@@ -5,13 +5,9 @@
 
 from Ressources.SyntheseVocaleSimple.tts import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    bot = Bot('en')
+    bot.speak("puta madre")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
