ffff74860990742abe92781c9591bd7a15120097
buckstarz <kevinchenier1995@gmail.com>
2021-03-23 23:07:26 -0400

    added new class for file reader

---
+++ fileReader.py
@@ -0,0 +1,14 @@
+import os.path
+
+class FileReader:
+
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self):
+        self.counter = 0
+        self.args = []
+
+    def getfiletext(self, file):
+        my_file = os.path.join(self.THIS_FOLDER, file)
+        text_file = open(my_file)
+        return text_file.read()
--- main.py
+++ main.py
@@ -4,19 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from Ressources.SyntheseVocaleSimple.tts import *
-import os.path
+from fileReader import *
 #Path to text file containing starting infrmations
 
 def getStartingInformations():
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, 'informations.txt')
 
-    startingInformations = open(my_file)
-    return startingInformations.read()
+    text_file = open(my_file)
+    return text_file.read()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     bot = Bot('en')
-    bot.speak(getStartingInformations())
+    file_reader = FileReader()
+
+    bot.speak(file_reader.getfiletext("informations.txt"))
     bot.speak("eren will kill mikasa")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
